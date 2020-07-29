4836a3b102106b3d242d8c363bf516b501a0d3e7
John Cokos <highlander.44@gmail.com>
2020-07-29 10:13:38 -0700

    pg server

--- finalExamApp/server.js
+++ finalExamApp/server.js
@@ -6,7 +6,6 @@ const superagent = require('superagent');
 require('ejs');
 const pg = require('pg');
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,22 +13,22 @@ app.use(express.urlencoded({ extended: true }));
 
 console.log(process.env.DATABASE_URL)
 
-const client = new pg.Client({connectionString:process.env.DATABASE_URL});
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
 client.on('error', err => console.error(err));
 
 app.get('/', getPoki);
 app.post('/add', addPoki);
 app.get('/favorites', getFavorites);
 
-function getPoki(req, res){
+function getPoki(req, res) {
   superagent.get('https://pokeapi.co/api/v2/pokemon')
     .then(results => {
       let sortedResults = results.body.results.sort((a, b) => a.name > b.name ? 1 : -1);
-      res.render('./show.ejs', {poki: sortedResults});
+      res.render('./show.ejs', { poki: sortedResults });
     })
 }
 
-function addPoki(request, res){
+function addPoki(request, res) {
   let name = request.body.name;
   let sql = `INSERT INTO pokimon (name) VALUES ($1) returning ID;`;
 
@@ -42,11 +41,11 @@ function addPoki(request, res){
     .catch((err) => console.error(err))
 }
 
-function getFavorites(req, res){
+function getFavorites(req, res) {
   let sql = 'SELECT * FROM pokimon;';
   client.query(sql)
     .then(results => {
-      res.render('./favorites', {poki: results.rows});
+      res.render('./favorites', { poki: results.rows });
     })
     .catch(err => console.log(err))
 }
