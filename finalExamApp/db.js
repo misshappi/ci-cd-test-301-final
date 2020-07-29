fd0e4327e740df6001ff3cc5760704fb20c4462f
John Cokos <highlander.44@gmail.com>
2020-07-29 11:10:27 -0700

    pg server

--- finalExamApp/db.js
+++ finalExamApp/db.js
@@ -7,11 +7,8 @@ const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
 client.on('error', err => console.error(err));
 client.connect();
 
-client.query('CREATE DATABASE finals')
+client.query('DROP TABLE IF EXISTS pokemon')
   .then(() => {
-    client.query('DROP TABLE IF EXISTS pokemon')
-      .then(() => {
-        client.query('CREATE TABLE pokemon ( id serial primary key, name varchar(255)')
-      })
-  });
+    client.query('CREATE TABLE pokemon ( id serial primary key, name varchar(255)')
+  })
 
